7e3d4f206563f624a2e63308e21c6966626d4e00
James Aules <james_aules@icloud.com>
2023-02-15 20:06:04 -0500

    iteration on title

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   let [data, setData] = useState({})
 
   useEffect(() => {
-    document.title = "Welcome to ArtWorld"
+    document.title = `Welcome to Museum of Art`
     fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${artId}`)
       .then(response => response.json())
       .then(resData => setData(resData))
@@ -23,6 +23,7 @@ function App() {
   return (
     <div className="App">
       <h1>ArtWorld</h1>
+      <h3>{data.title}</h3>
       <Gallery
         objectImg={data.primaryImage}
         artist={data.artistDisplayName}
